9559c3feea6e5243d16b28bab1360a35f98ae4f0
Nihad KV <nihad083@gmail.com>
2021-07-25 17:42:29 +0530

    pagination for infinite scroll api implemented

--- src/feed/services/feed/feed.service.ts
+++ src/feed/services/feed/feed.service.ts
@@ -20,8 +20,15 @@ export class FeedService {
     return from(this.feedPostRepository.find());
   }
 
+  findPost(take: number = 10, skip: number = 0): Observable<FeedPost[]> {
+    return from(
+      this.feedPostRepository.findAndCount({ take, skip }).then(([posts]) => {
+        return <FeedPost[]>posts;
+      }),
+    );
+  }
+
   updatePost(id: number, feedPost: FeedPost): Observable<UpdateResult> {
-    console.log(id, feedPost);
     return from(this.feedPostRepository.update(id, feedPost));
   }
 
